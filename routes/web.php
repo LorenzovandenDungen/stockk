d219f034b34775943e771e8bf2e9fad730b9befe
LorenzovandenDungen <2130766@talnet.nl>
2024-02-02 11:05:21 +0100

    save

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('stocks', [App\Http\Controllers\StockController::class, 'index']);
+Route::get('stocks/create', [App\Http\Controllers\StockController::class, 'create']);
 
 Route::get('/', function () {
     return view('welcome');
