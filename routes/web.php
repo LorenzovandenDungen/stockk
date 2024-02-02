26b7431789742d4c025d966763cf866cb375592a
LorenzovandenDungen <2130766@talnet.nl>
2024-02-02 10:48:33 +0100

    save

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('stocks', [App\Http\Controllers\StockController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
