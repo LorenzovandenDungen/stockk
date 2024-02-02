6d75dc422d2a1650d075ff4f8c88e2655c8d9223
LorenzovandenDungen <2130766@talnet.nl>
2024-02-02 11:16:41 +0100

    save

--- resources/views/stocks/index.blade.php
+++ resources/views/stocks/index.blade.php
@@ -8,30 +8,34 @@
     <title>Stocks</title>
 </head>
 <body>
-  <div class="container" style="margin:40px;">
-    <table class="table">
-      <thead class="thead-light">
-          <tr>
-            <th>ID</th>
-            <th>Stock Name</th>
-            <th>Stock Ticket</th>
-            <th>Stock Value</th>
-            <th>Updated at</th>
-          </tr>
-      </thead>
-      <tbody>
-          @foreach($stocks as $stock)
-          <tr>
-              <td>{{$stock->id}}</td>
-              <td>{{$stock->stock_name}} </td>
-              <td>{{$stock->ticket}}</td>
-              <td>{{$stock->value}}</td>
-              <td>{{$stock->updated_at}}</td>
-          </tr>
-          @endforeach
-      </tbody>
-    </table>
-  </div>
-
+    <div class="container" style="margin: 40px;">
+        <h1 class="display-3">Stocks</h1>
+        <table class="table">
+            <thead class="thead-light">
+                <tr>
+                    <th>ID</th>
+                    <th>Stock Name</th>
+                    <th>Stock Ticket</th>
+                    <th>Stock Value</th>
+                    <th>Updated at</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($stocks as $stock)
+                <tr>
+                    <td>{{$stock->id}}</td>
+                    <td>{{$stock->stock_name}}</td>
+                    <td>{{$stock->ticket}}</td>
+                    <td>{{$stock->value}}</td>
+                    <td>{{$stock->updated_at}}</td>
+                    <td>
+                        <a href="/stocks/edit/{{$stock->id}}" class="btn btn-primary">Edit</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
